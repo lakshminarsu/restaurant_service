6e20a188e86bf582bd002bcc027fe9c232a1efe6
lakshminarsu <lakshminarsu.tunuguntla@cerner.com>
2023-09-03 14:41:11 +0530

    sql script changes

---
+++ src/main/sql/script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE `restaurant` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `main_menu` (
+  `id` int NOT NULL,
+  `item_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `item_name_UNIQUE` (`item_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `menu_items` (
+  `id` int NOT NULL,
+  `main_menu_item_id` int NOT NULL,
+  `item_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `main_menu_item_id` (`main_menu_item_id`),
+  CONSTRAINT `menu_items_ibfk_1` FOREIGN KEY (`main_menu_item_id`) REFERENCES `main_menu` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
